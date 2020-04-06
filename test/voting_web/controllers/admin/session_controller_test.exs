d823459af2664d7ca2eff12fbfa86b4b3659408e
Guilherme Nogueira <nogueig@gmail.com>
2020-04-06 08:03:26 -0300

    Adding tests

---
+++ test/voting_web/controllers/admin/session_controller_test.exs
@@ -0,0 +1,34 @@
+defmodule VotingWeb.Admin.SessionControllerTest do
+  use VotingWeb.ConnCase, async: true
+
+  import Voting.Factory
+
+  describe "create/2" do
+
+    setup %{conn: conn} do
+      insert(:admin, name: "John Smith", email: "john@gmail.com")
+      %{conn: conn}
+    end
+
+    test "returns 200 when credentials are valid" do
+      res = post(build_conn(), "/api/v1/admin/sign_in", %{"email" => "john@gmail.com", "password" => "123456"})
+      assert %{
+               "status" => "ok",
+               "data" => %{
+                 "name" => "John Smith"
+               }
+             } = json_response(res, 200)
+    end
+
+    test "returns 401 when email is invalid" do
+      res = post(build_conn(), "/api/v1/admin/sign_in", %{"email" => "johnX@gmail.com", "password" => "123456"})
+      assert %{"status" => "unauthenticated"} = json_response(res, 401)
+    end
+
+    test "returns 401 when password is invalid" do
+      res = post(build_conn(), "/api/v1/admin/sign_in", %{"email" => "john@gmail.com", "password" => "X123456"})
+      assert %{"status" => "unauthenticated"} = json_response(res, 401)
+    end
+
+  end
+end
